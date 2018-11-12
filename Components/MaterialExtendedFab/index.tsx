121b3f865a868e2587af5190c2c67b857bc47aa6
Sajjad Asadi <lvlr.xaus@gmail.com>
2018-11-13 02:59:21 +0330

    added components

--- Components/MaterialExtendedFab/index.tsx
+++ Components/MaterialExtendedFab/index.tsx
@@ -1,38 +1,41 @@
 import * as React from "react";
 import Ripple from 'react-native-material-ripple';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Text} from "react-native";
 
-export interface MaterialFabProps {
+export interface MaterialExtendedFabProps {
   icon?: React.ReactNode,
   color?:string,
   iconColor?: string,
   onPress?():void,
   iconName:string,
-  mini?:boolean,
+  textFont?:string,
+  text:string,
   position:string//{'bottomLeft','topRight','topLeft','bottomRight'}
 }
 
-export default (props: MaterialFabProps) =>
+export default (props: MaterialExtendedFabProps) =>
   <Ripple
     rippleColor={props.iconColor}
     onPress={()=>props.onPress()}
-    style={{borderRadius: props.mini?20:28,
-      height:props.mini? 40 : 56,
-      width: props.mini? 40 : 56,
+    style={{borderRadius: 28,
+      height: 56,
       backgroundColor:props.color,
       overflow:'hidden',
+      paddingLeft:16,
+      paddingRight:16,
+      flexDirection:'row',
       elevation:16,
-      left:props.position === "bottomLeft"|| props.position === "topLeft" ? 16:undefined,
-      right:props.position === "bottomRight"|| props.position === "topRight" ? 16:undefined,
-      top:props.position === "topRight"|| props.position === "topLeft" ? 16:undefined,
-      bottom:props.position === "bottomRight"|| props.position === "bottomLeft" ? 16:undefined,
+      top:props.position === "top" ? 16:undefined,
+      bottom:props.position === "bottom" ? 16:undefined,
       position:'absolute',
       alignItems: 'center',
       justifyContent: 'center'}}>
     {props.icon ? props.icon :
       props.iconName &&
       <MaterialCommunityIcons
-        style={{margin:8}} name={props.iconName} size={24} color= {props.iconColor}
+        style={{marginRight:8}} name={props.iconName} size={24} color= {props.iconColor}
       />
     }
+    <Text style={{fontFamily:props.textFont,color:props.iconColor,fontSize:14}}>{props.text}</Text>
   </Ripple>;
--- index.ts
+++ index.ts
@@ -7,12 +7,14 @@ import MaterialContainer from './Components/MaterialContainer';
 import MaterialHorizontalLinear from './Components/MaterialHorizontalLinear';
 import MaterialVerticalLinear from './Components/MaterialHorizontalLinear';
 import MaterialColors from './Utils/MaterialColors';
+import MaterialExtendedFab from './Components/MaterialExtendedFab';
 
 export {
     MaterialTextButton,
     MaterialOutlinedButton,
     MaterialContainedButton,
     MaterialFab,
+    MaterialExtendedFab,
     MaterialTextInput,
     MaterialContainer,
     MaterialHorizontalLinear,
